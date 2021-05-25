b99c8bf637ea7144dad0cdda3877bfaeddb14e78
Novailoveyou <novailoveyoux@gmail.com>
2021-05-25 18:51:31 +0300

    added gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/app.sass'
+import TagManager from 'react-gtm-module'
+import { gtmId } from '@/config/index'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId, dataLayerName: 'dataLayer' })
+  }, [])
+
   return <Component {...pageProps} />
 }
 
