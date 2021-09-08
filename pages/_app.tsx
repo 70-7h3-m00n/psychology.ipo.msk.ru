f61f7f7bbbd0e1a8550b5d4577919465ff9a2690
novailoveyou <novailoveyoux@gmail.com>
2021-09-08 14:58:09 +0300

    replaced old way of static js scripts to new way of Script tag next11

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,25 @@ import '@/styles/app.sass'
 import TagManager from 'react-gtm-module'
 import { gtmId } from '@/config/index'
 import { useEffect } from 'react'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     TagManager.initialize({ gtmId, dataLayerName: 'dataLayer' })
   }, [])
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script src='https://code.jquery.com/jquery-3.6.0.min.js' />
+      <Script src='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js' />
+      <Script src='https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js' />
+      <Script src='https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.0/jquery.magnific-popup.min.js' />
+      <Script src='https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js' />
+      <Script src='/assets/js/sberSdk.js' />
+      <Script src='/assets/js/myjs.js' />
+    </>
+  )
 }
 
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,33 +21,6 @@ class MyDocument extends Document {
             name='facebook-domain-verification'
             content='8dw7ic38j9fzri38ak0xd5scopqo4k'
           />
-
-          <script
-            src='https://code.jquery.com/jquery-3.6.0.min.js'
-            integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4='
-            crossOrigin='anonymous'></script>
-          <script
-            src='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js'
-            integrity='sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg=='
-            crossOrigin='anonymous'
-            referrerPolicy='no-referrer'></script>
-          <script
-            src='https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js'
-            integrity='sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ=='
-            crossOrigin='anonymous'
-            referrerPolicy='no-referrer'></script>
-          <script
-            src='https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.0/jquery.magnific-popup.min.js'
-            integrity='sha512-+m6t3R87+6LdtYiCzRhC5+E0l4VQ9qIT1H9+t1wmHkMJvvUQNI5MKKb7b08WL4Kgp9K0IBgHDSLCRJk05cFUYg=='
-            crossOrigin='anonymous'
-            referrerPolicy='no-referrer'></script>
-          <script
-            src='https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js'
-            integrity='sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g=='
-            crossOrigin='anonymous'
-            referrerPolicy='no-referrer'></script>
-          <script type='text/javascript' src='/assets/js/sberSdk.js'></script>
-          <script type='text/javascript' src='/assets/js/myjs.js'></script>
         </Head>
         <body>
           <Main />
